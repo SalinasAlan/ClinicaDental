22bbd35dcdd1e3340071cc10c451beb3a0b7efad
César Álvarez <36392460+Cesar98@users.noreply.github.com>
2019-11-29 13:18:57 -0600

    Se borró lo de la base de datos

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 session_start();
-require 'database.php';
-if (isset($_SESSION['user_id'])) {
-    $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
-    $records->bindParam(':id', $_SESSION['user_id']);
-    $records->execute();
-    $results = $records->fetch(PDO::FETCH_ASSOC);
-    $user = null;
-    if (count($results) > 0) {
-        $user = $results;
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
