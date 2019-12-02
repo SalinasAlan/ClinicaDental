06ff613787504d37baf8d571b77d0f2ca5ea42fe
Alan <alansal_11@hotmail.com>
2019-12-01 18:09:32 -0600

    Se modificó el login

--- login.php
+++ login.php
@@ -1,71 +1,49 @@
-
-<?php
-  session_start();
-  if (isset($_SESSION['user_id'])) {
-    header('Location: /Tarea/indexEmploy.php');
-  }
-  require 'database.php';
-  if (!empty($_POST['email']) && !empty($_POST['password'])) {
-    $records = $conn->prepare('SELECT id, email, password FROM users WHERE email = :email');
-    $records->bindParam(':email', $_POST['email']);
-    $records->execute();
-    $results = $records->fetch(PDO::FETCH_ASSOC);
-    $message = '';
-    if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
-      $_SESSION['user_id'] = $results['id'];
-      header("Location: /Tarea/indexEmploy.php");
-    } else {
-      $message = 'Sorry, those credentials do not match';
-    }
-  }
-?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Login</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    </head>
-    <body>
 
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="index.php">Clinica Dental "Da BlackTeam"</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="index.php"><span class="glyphicon glyphicon-open"> Registrar Usuario </a></li>
-                    <li><a href="index.php"><span class="glyphicon glyphicon-plus"> Agendar Nueva Cita </a></li>
-                    <li><a href="index.php"><span class="glyphicon glyphicon-th-list"> Agenda General </a></li>
-                    <li><a href="index.php"><span class="glyphicon glyphicon-list-alt"> Citas Agendadas </a></li>
-                </ul>
+<head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Clinica Dental "Da BlackTeam"</a>
             </div>
-        </nav>
-
-        <div>
-        <?php if(!empty($message)): ?>
-      <p> <?= $message ?></p>
-    <?php endif; ?>
-
-    <h1>Iniciar sesion</h1>
-    <span>o <a href="singup.php">Registrarme</a></span>
-
-    <form action="login.php" method="POST">
-      <input name="email" type="text" placeholder="Ingresa tu email">
-      <input name="password" type="password" placeholder="Ingresa tu contraseña">
-      <input type="submit" value="Iniciar sesion">
-    </form>
         </div>
-    </body>
+    </nav>
 
-    <footer>
-        <p class="texto">Cuar es un nickname tan reservado como la pagina y su mismo creador.</p>
-    </footer>
+    <div class="wrapper fadeInDown" style="margin-top: -80px;">
+        <div id="formContent">
+            <form>
+                <h1 style="margin-bottom: 20px;">Login</h1>
+                <input type="text" id="usuario" class="fadeIn second" name="usuario" placeholder="usuario" required>
+                <input type="password" id="contrasenia" class="fadeIn third" name="contrasenia" placeholder="contraseña"
+                    required>
+                <input type="submit" class="fadeIn fourth" value="Log In">
+            </form>
+        </div>
+    </div>
+</body>
+
+<footer class="bg-dark">
+    <p class="texto">Cuar es un nickname tan reservado como la pagina y su mismo creador.</p>
+</footer>
+
 </html>
