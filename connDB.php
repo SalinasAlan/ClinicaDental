d205b4e9f55822b502fe9e693885e7d53167edaf
Alan <alansal_11@hotmail.com>
2019-12-01 18:09:46 -0600

    conn a la base de datos

---
+++ connDB.php
@@ -0,0 +1,14 @@
+<?php
+
+    $servidor = "localhost";
+    $usuario = "root";
+    $pass = "";
+    $db = "dental";
+
+    $conn = new mysqli($servidor, $usuario, $pass, $db);    
+    
+    
+    // if ($conn) {
+    //     echo 'connected';
+    // }
+?>
